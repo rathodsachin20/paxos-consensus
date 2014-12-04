50a4251b61c8b30bccd809839f5fcb1c47c33234
Sachin Rathod <rathodsachin20@gmail.com>
2014-12-03 23:38:08 -0800

    Added paxos server functions.

--- paxos.py
+++ paxos.py
@@ -1,19 +1,79 @@
-from datalog import DataLog
+#from datalog import DataLog
+import socket
+import thread
 
-class Net:
-    def listen(port):
-
-    def send(message, iplist):
-
-    def handle_response(response):
-
+IP = '127.0.0.1'
+PORT = 27100
+BUFFER_SIZE = 64
 
 class Paxos:
 
-    dl = DataLog
+#    dl = DataLog()
 
+    def resp_handler(self, client_sock, addr):
+        while 1:
+            data = client_sock.recv(BUFFER_SIZE)
+            if not data:
+                break
+            else:
+                print data
+                if data=="PREPARE":
+                    msg = "Msg from server: Got PREPARE"
+                elif data=="ACCEPT":
+                    msg = "Msg from server: Got - ACCEPT"
+                elif data=="DECIDE":
+                    msg = "Msg from server: Got - DECIDE"
+                else:
+                    msg = "Msg from server: Got data - %s" % data
+                client_sock.send(msg)
+        client_sock.close()
+
+    def start_server(self):
+        try:
+            self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server_sock.bind((IP, PORT))
+            self.server_sock.listen(1)
+            while 1:
+                sock, addr = self.server_sock.accept()
+                thread.start_new_thread(self.resp_handler, (sock, addr))
+        except KeyboardInterrupt:
+            print "Closing Server!"
+        except Exception as ex:
+            print "Exception occurred in start_server: %s" % ex
+        finally:
+            # TODO: save paxos state to disk
+            if self.server_sock:
+                self.server_sock.close()
+
+    def send_single(self, message, ip):
+        try:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect((ip, PORT))
+            client.send(message)
+        except Exception as ex:
+            print "Exception occurred in send_single: %s" % ex
+        finally:
+            if client:
+                client.close()
+
+    def send_to_all(self, message, iplist):
+        try:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            for ip in iplist:
+                client.connect((ip, PORT))
+                client.send(message)
+                client.close()
+        except Exception as ex:
+            print "Exception occurred in send_single: %s" % ex
+        finally:
+            if client:
+                client.close()
+
+"""
     # Initialize log
     def init_system():
+
+    def update_log():
 
     def deposit(value):
 
@@ -26,3 +86,6 @@ class Paxos:
     def broadcast(value):
 
     def recover():
+"""
+p = Paxos()
+p.start_server()
