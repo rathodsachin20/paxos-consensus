22bdd435b978413d67bf92192711b8189d54fa5b
Sachin Rathod <rathodsachin20@gmail.com>
2014-12-03 20:54:21 -0800

    initial functions outline.

---
+++ paxos.py
@@ -0,0 +1,28 @@
+from datalog import DataLog
+
+class Net:
+    def listen(port):
+
+    def send(message, iplist):
+
+    def handle_response(response):
+
+
+class Paxos:
+
+    dl = DataLog
+
+    # Initialize log
+    def init_system():
+
+    def deposit(value):
+
+    def withdraw(value):
+
+    def balance():
+
+    def get_log_position():
+
+    def broadcast(value):
+
+    def recover():
