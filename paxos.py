702dbe0050d4c43bbf416c1d67c08fdcb8b2495f
Sachin Rathod <rathodsachin20@gmail.com>
2014-12-05 23:46:32 -0800

    paxos working with two nodes.

--- paxos.py
+++ paxos.py
@@ -12,6 +12,8 @@ class Paxos:
 
 #    dl = DataLog()
     def __init__(self, listen_port=PORT, ip_list=None, port_list=None):
+        self.data = [None]*20
+        self.latest_log_position = -1
         self.listen_port = listen_port
         self.ip_list = ip_list
         self.port_list = port_list
@@ -19,9 +21,12 @@ class Paxos:
         self.accept_num = 0
         self.accept_val = (-1, -1.0) # (logposition, logentry)
         self.my_val = (-1, -1.0)
-        self.majority = len(ip_list)/2 + 1
+        self.majority = len(ip_list)/2 
+        self.ack_count = 0
         self.highest_bal = -1
-        self.highet_val = (-1 -1.0)
+        self.highest_val = (-1, -1.0)
+        self.accepted_highest_bal = -1
+        self.accept_count = 0
 
     def get_prepare_response(self, bal):
         if bal >= self.ballot_num:
@@ -31,25 +36,66 @@ class Paxos:
             reply = "NACK:" + str(bal),":" + str(self.accept_num) + ":" + str(self.accept_val)
         return str(reply)
 
-    def get_ack_reponse(data):
+    def handle_ack(self, data):
         data_list = data.split(':')
         bal = int(data_list[1])
+        print self.ballot_num, bal
         if bal != self.ballot_num: # Recd older ballot ack
-            return None
+            return
         if data_list[0]=="NACK":
             #start new rouund
             pass
-        else:
+        elif data_list[0]=="ACK":
             accept_num = int(data_list[2])
             accept_val = eval(data_list[3])
             if self.highest_bal < accept_num:
                 self.highest_bal = accept_num
                 self.highest_val = accept_val
             self.ack_count += 1
+            print "ack count ", self.ack_count, " majority", self.majority
             if(self.ack_count >= self.majority):
-                if !self.highest_val == (-1, -1.0):
+                print "Got majority acks! Thanks guys."
+                if not(self.highest_val == (-1, -1.0)):
                     self.my_val = self.highest_val
-                
+                print "sending accept"
+                msg = str("ACCEPT:" + str(self.ballot_num) + ":" + str(self.my_val))
+                self.send_to_all(msg, self.ip_list, self.port_list)
+                self.accept_num = self.ballot_num
+                self.accept_val = self.my_val
+                self.ack_count = 0
+                self.accepted_highest_bal = self.accept_num
+                self.accept_count = 1
+        else:
+            return
+
+    def handle_accept(self, data):
+        data_list = data.split(':')
+        print data, self.accepted_highest_bal
+        bal = int(data_list[1])
+        val = eval(data_list[2])
+        if bal > self.accepted_highest_bal:
+            self.accepted_highest_bal = bal
+            self.accept_count = 1
+        elif bal == self.accepted_highest_bal:
+            self.accept_count += 1
+        else:
+            return # Ignore!!!
+
+        if bal >= self.ballot_num and self.accept_count == 1:
+            self.accept_num = bal
+            self.accept_val = val
+            self.ballot_num = bal # ?????
+            msg = str("ACCEPT:" + str(bal) + ":" + str(val))
+            self.send_to_all(msg, self.ip_list, self.port_list)
+        if self.accept_count >= self.majority:
+            print "deciding on:", val
+            self.data[val[0]] = val[1]
+            self.latest_log_position += 1
+            self.accept_num = 0
+            self.ballot_num = 0
+            self.accept_val = (-1, -1.0)
+            self.accepted_highest_bal = -1
+            print "Current Data:", self.data
 
     def req_handler(self, client_sock, addr):
         while 1:
@@ -70,9 +116,9 @@ class Paxos:
                     self.send_single(resp, ip=self.ip_list[0], port=self.port_list[0])
                     #client_sock.send(resp)
                 elif data.startswith("ACK") or data.startswith("NACK"):
-                    self.get_ack_reponse(data)
+                    self.handle_ack(data)
                 elif data.startswith("ACCEPT"):
-                    msg = "Msg from server: Got - ACCEPT"
+                    self.handle_accept(data)
                 elif data.startswith("DECIDE"):
                     msg = "Msg from server: Got - DECIDE"
                 else:
@@ -114,15 +160,15 @@ class Paxos:
             if client:
                 client.close()
 
-    def send_to_all(self, message, iplist, port=PORT):
+    def send_to_all(self, message, ip_list, port_list):
         try:
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            for ip in iplist:
+            for ip, port in zip(ip_list, port_list):
+                client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 client.connect((ip, port))
                 client.send(message)
                 client.close()
         except Exception as ex:
-            print "Exception occurred in send_single: %s" % ex
+            print "Exception occurred in send_to_all: %s" % ex
         finally:
             if client:
                 client.close()
@@ -147,7 +193,7 @@ class Paxos:
 """
 try:
     print sys.argv[1]
-    p = Paxos(listen_port=int(sys.argv[1]), ip_list=['127.0.0.1'], port_list=[int(sys.argv[2])])
+    p = Paxos(listen_port=int(sys.argv[1]), ip_list=['127.0.0.1', '127.0.0.1'], port_list=[int(sys.argv[2]), int(sys.argv[3])])
     #thread.start_new_thread(p.start_server, ())
     tt = threading.Thread(target=p.start_server)
     tt.setDaemon(True)
@@ -157,8 +203,13 @@ try:
     #tt.join()
     while 1:
         var = raw_input("Enter Command:")
-        if var=="send":
-            p.send_single("PREPARE:5", '127.0.0.1', int(sys.argv[2]))
+        if var.startswith("d"):
+            varlist = var.split(' ')
+            p.ballot_num+=1
+            p.my_val = (p.latest_log_position+1, varlist[1])
+            msg = str("PREPARE:"+str(p.ballot_num))
+            p.send_to_all(msg, p.ip_list, p.port_list)
+            p.data
         #time.sleep(1)
 except KeyboardInterrupt:
     print "Closing Server!"
