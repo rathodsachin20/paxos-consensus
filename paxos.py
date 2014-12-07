ae8177a26a1193323167318da04411ddb92389ba
Sahaj Biyani <sahaj@Sahajs-MacBook-Pro.local>
2014-12-06 17:15:40 -0800

    working wid 3 servers

--- paxos.py
+++ paxos.py
@@ -12,8 +12,9 @@ class Paxos:
 
 #    dl = DataLog()
     def __init__(self, listen_port=PORT, ip_list=None, port_list=None):
-        self.data = [None]*5
+        self.data = [None]*15
         self.latest_log_position = -1
+        self.latest_decided_val = (-1, -1.0)
         self.listen_port = listen_port
         self.ip_list = ip_list
         self.port_list = port_list
@@ -22,16 +23,31 @@ class Paxos:
         self.accept_val = (-1, -1.0) # (logposition, logentry)
         self.my_val = (-1, -1.0)
         self.majority = len(ip_list)/2 + 1
-        self.ack_count = 1
-        self.highest_bal = -1
-        self.highest_val = (-1, -1.0)
+        self.ack_count = 0
+        self.highest_accept_num = -1
+        self.highest_accept_val = (-1, -1.0)
         self.accepted_highest_bal = -1
         self.accept_count = 0
         self.max_accept = 3
         self.sent_accept_to_all = 0
+	self.recd_maj_acks = False
+	self.decided = False
+	self.decide_lock = threading.Lock()
+	self.accept_lock = threading.Lock()
+
+    def prepare(self, val):
+        self.ack_count = 1
+	self.highest_accept_num = -1
+	self.highest_accept_val = (-1, -1.0)
+        self.my_val = val
+        self.ballot_num += 1
+	self.recd_maj_acks = False
+	self.decided = False
+        msg = str("PREPARE:"+str(self.ballot_num)+":"+str(p.listen_port))
+        self.send_to_all(msg, self.ip_list, self.port_list)
 
     def get_prepare_response(self, bal):
-        if bal >= self.ballot_num:
+        if bal > self.ballot_num:
             self.ballot_num = bal
             reply = "ACK:" + str(bal) + ":" + str(self.accept_num) + ":" + str(self.accept_val)
         else:
@@ -51,24 +67,27 @@ class Paxos:
         elif data_list[0]=="ACK":
             accept_num = int(data_list[2])
             accept_val = eval(data_list[3])
-            if self.highest_bal < accept_num:
-                self.highest_bal = accept_num
-                self.highest_val = accept_val
+            if self.highest_accept_num < accept_num:
+                self.highest_accept_num = accept_num
+                self.highest_accept_val = accept_val
             self.ack_count += 1
-            print "ack count ", self.ack_count, " majority", self.majority
-            if(self.ack_count >= self.majority):
-                print "Got majority acks! Thanks guys."
-                if not(self.highest_val == (-1, -1.0)):
-                    self.my_val = self.highest_val
-                print "sending accept"
-                msg = str("ACCEPT:" + str(self.ballot_num) + ":" + str(self.my_val))
-                self.send_to_all(msg, self.ip_list, self.port_list)
-                self.sent_accept_to_all = 1
-                self.accept_num = self.ballot_num
-                self.accept_val = self.my_val
-                self.ack_count = 1
-                self.accepted_highest_bal = self.accept_num
-                self.accept_count = 1
+	    with self.accept_lock:
+                print "ack count ", self.ack_count, " majority", self.majority
+	        print self.ack_count==self.majority
+                if(self.ack_count >= self.majority and not self.recd_maj_acks):
+                    print "Got majority acks! Thanks guys."
+                    self.recd_maj_acks = True
+                    if not(self.highest_accept_val == (-1, -1.0)):
+                        self.my_val = self.highest_accept_val
+                    print "sending accept"
+                    msg = str("ACCEPT:" + str(self.ballot_num) + ":" + str(self.my_val))
+                    self.send_to_all(msg, self.ip_list, self.port_list)
+                    self.accept_count = 1
+                    #self.sent_accept_to_all = 1
+                    self.accept_num = self.ballot_num
+                    self.accept_val = self.my_val
+                    #self.accepted_highest_bal = self.accept_num
+                    #self.ack_count = 1
 
         else:
             return
@@ -76,19 +95,25 @@ class Paxos:
     def handle_accept(self, data):
         print "in HANDLE ACCEPT"
         data_list = data.split(':')
-        print data, self.accepted_highest_bal
+        print data, self.accept_num
         bal = int(data_list[1])
         val = eval(data_list[2])
-        if bal > self.accepted_highest_bal:
-            self.accepted_highest_bal = bal
+	if val <= self.latest_decided_val:
+	    return
+        if bal > self.accept_num:
+            self.accept_num = bal
+	    self.accept_val = val
             self.accept_count = 1
-        elif bal == self.accepted_highest_bal:
+	    self.decided = False
+            msg = str("ACCEPT:" + str(bal) + ":" + str(val))
+            self.send_to_all(msg, self.ip_list, self.port_list)
+        elif bal == self.accept_num:
             self.accept_count += 1
             print "Accept count is: ", self.accept_count
         else:
             return # Ignore!!!
 
-        if bal >= self.ballot_num and self.sent_accept_to_all == 0:
+        '''if bal >= self.ballot_num and self.sent_accept_to_all == 0:
             self.sent_accept_to_all = 1
             print "send ACCEPT to ALL only ONCE!"
             self.accept_num = bal
@@ -96,15 +121,22 @@ class Paxos:
             self.ballot_num = bal # ?????
             msg = str("ACCEPT:" + str(bal) + ":" + str(val))
             self.send_to_all(msg, self.ip_list, self.port_list)
-        
-        if self.accept_count >= self.majority and self.accept_count < self.max_accept :
+        '''
+	with self.decide_lock:
+            print "decided", self.decided
+            if self.accept_count >= self.majority and not self.decided:
+                self.decide(bal, val)
+
+    def decide(self, bal, val):
             print "DECIDED ON:", val
+            self.decided = True
             self.data[val[0]] = val[1]
             self.latest_log_position += 1
+	    self.latest_decided_val = val
             self.accept_num = 0
-            self.ballot_num = 0
             self.accept_val = (-1, -1.0)
-            self.accepted_highest_bal = -1
+            self.ballot_num = 0
+	    self.accept_count = 0
             print "Current Data:", self.data
             print "\n \n"
 
@@ -219,10 +251,12 @@ try:
         var = raw_input("Enter Command:")
         if var.startswith("d"):
             varlist = var.split(' ')
-            p.ballot_num+=1
-            p.my_val = (p.latest_log_position+1, varlist[1])
-            msg = str("PREPARE:"+str(p.ballot_num)+":"+str(p.listen_port))
-            p.send_to_all(msg, p.ip_list, p.port_list)
+            #p.ballot_num+=1
+            val = (p.latest_log_position+1, varlist[1])
+            #msg = str("PREPARE:"+str(p.ballot_num)+":"+str(p.listen_port))
+            #p.send_to_all(msg, p.ip_list, p.port_list)
+	    p.prepare(val)
+	    time.sleep(3)
             p.data
         #time.sleep(1)
 except KeyboardInterrupt:
